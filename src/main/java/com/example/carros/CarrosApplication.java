13f334282d64982682cf0326bbe440cdbe6f3d3a
Cristian <cristian.ms@hotmail.com>
2020-04-13 08:22:22 -0300

    Criação da API com Spring Boot initializer e controller básico

---
+++ src/main/java/com/example/carros/CarrosApplication.java
@@ -0,0 +1,14 @@
+package com.example.carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class CarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosApplication.class, args);
+	}
+
+}
