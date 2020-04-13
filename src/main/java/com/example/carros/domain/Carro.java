6b3c6fc0c1c21dbde87028c398588984ff53928c
Cristian <cristian.ms@hotmail.com>
2020-04-13 10:10:44 -0300

    Adicionadas depedências para conexão com o banco de dados e alterado o controller para acesso aos dados da tabela

--- src/main/java/com/example/carros/domain/Carro.java
+++ src/main/java/com/example/carros/domain/Carro.java
@@ -1,10 +1,31 @@
 package com.example.carros.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Carro {
 
+    @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO) // Para auto increment de outros bancos
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Para auto increment do MySQL
     private Long id;
+
     private String nome;
 
+    /**
+     * Construtor padrão necessário para o Springboot
+     */
+    public Carro() {
+    }
+
+    /**
+     * Construtor
+     * @param id Identificador do carro
+     * @param nome Nome do carro
+     */
     public Carro(Long id, String nome) {
         this.id = id;
         this.nome = nome;
--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -1,17 +1,34 @@
 package com.example.carros.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarroService {
 
-    public List<Carro> getCarros() {
-        List<Carro> carros = new ArrayList<>();
+    @Autowired
+    private CarroRepository rep;
 
+    /**
+     * Retorna a lista de carros completa do banco de dados
+     * @return Iterable<Carro>
+     */
+    public Iterable<Carro> getCarros() {
+        return this.rep.findAll();
+    }
+
+    /**
+     * Retorna a lista de carros criada em memória
+     * @return List<Carro>
+     */
+    public List<Carro> getCarrosFake() {
+        List<Carro> carros = new ArrayList<>();
         carros.add(new Carro(1L, "Fusca"));
         carros.add(new Carro(2L, "Brasília"));
         carros.add(new Carro(3L, "Chevete"));
-
         return carros;
     }
 }
